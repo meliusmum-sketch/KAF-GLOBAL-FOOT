3879a95c55b1d63799d583947396f2446cbad28e
meliusmum-sketch <meliusmum@gmail.com>
2025-11-25 05:44:30 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,19 @@
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      {/* Bouton WhatsApp flottant, visible sur toutes les pages */}
+      <a
+        href="https://wa.me/221782702457"
+        className="whatsapp-float"
+        target="_blank"
+        rel="noreferrer"
+      >
+        WhatsApp
+      </a>
+    </>
+  );
 }
